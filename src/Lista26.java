f338e8ccc908d8f923a08febc0d234a8f65265bc
user <user@user-PC161114>
2017-02-28 16:08:29 -0300

    28-02-16h

--- src/Lista26.java
+++ src/Lista26.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 /*
 Elaborar um programa, em linguagem Java, que utilize apenas métodos recursivos,
 capaz de receber, via teclado, dois números inteiros e positivos, calculando o Máximo
@@ -6,8 +8,33 @@ Divisor Comum entre eles.
 public class Lista26 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int n1 = Integer.parseInt(JOptionPane.showInputDialog("digite n1 maior que 0:"));
+		int n2 = Integer.parseInt(JOptionPane.showInputDialog("digite n2 maior que 0:"));
+		int cont;
+		
+			if(n1>=n2){
+				cont  = n1-1;
+			}else{
+				cont = n2-1;
+			}
+				
+		JOptionPane.showMessageDialog(null, "O Maximo divisor comum é " + MaxDivComum(n1,n2, cont));
+		
+	}
+	
+		
+	public static int MaxDivComum(int n1, int n2, int cont){
+		
+		if(cont<=0){
+			return 1;
+		}
+		
+		if(n1%cont==0 && n2%cont==0){
+			return cont;
+		}
+		return MaxDivComum(n1, n2, cont-=1);
+		
 	}
 
 }
--- src/Lista27.java
+++ src/Lista27.java
@@ -1,9 +1,42 @@
+import javax.swing.JOptionPane;
 
+/*
+
+Elaborar um programa, em linguagem Java, que utilize apenas métodos recursivos,
+capaz de receber, via teclado, dois números inteiros e positivos, calculando o primeiro
+elevado ao segundo.
+
+*/
 public class Lista27 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	int n = Integer.parseInt(JOptionPane.showInputDialog("digite n1"));
+	int n2 = Integer.parseInt(JOptionPane.showInputDialog("digite n2"));
+	int cont = 0;
+	int result = 1;
+	
+	JOptionPane.showMessageDialog(null, n+" elevado a "+n2+" igual a "+ calculaElevado(n,n2, cont, result));
 
+	}
+	
+	public static int calculaElevado(int n, int n2, int cont, int result){
+		if(n2==0)
+			return result;
+		
+		if(n<0)
+			return 99999999;
+		
+		if(cont==n2){
+			//result *= n;
+			return result;
+		}
+		
+		if(cont<n2){
+			result *= n;
+			return calculaElevado(n, n2, cont+=1, result);
+		}else
+			return 9999;
 	}
 
 }
